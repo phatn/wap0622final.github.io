943734a3bf2a3d56020f37f76e2e433d18754f52
Phat Nguyen <phatnguyentanit@gmail.com>
2022-07-10 21:44:45 -0500

    Fixed

--- server/util/Util.js
+++ server/util/Util.js
@@ -2,7 +2,15 @@ module.exports = class Util {
 
     static getUsername(req) {
         const authorization = req.headers.authorization;
-        const accessToken = authorization.split(' ')[1];
-        return accessToken.split('_')[0];
+        const authorizationArr = authorization.split(' ');
+        if(authorizationArr.length > 1) {
+            const accessToken = authorizationArr[1];
+            const accessTokenArr = accessToken.split('_');
+            if(accessTokenArr.length > 0) {
+                return accessTokenArr[0];
+            }
+        }
+
+        return '';
     }
 }
